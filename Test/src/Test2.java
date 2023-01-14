8f74bf6e0bc1f8faceb6c5820a0e08744cae739d
patient-fyd <patient.fyd@gmail.com>
2023-01-15 03:03:54 +0800

    test2
    test2

---
+++ Test/src/Test2.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Fourteen-Y
+ * @Description:
+ * @DateTime: 2023/1/15 3:03
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println(1+2);
+    }
+}
