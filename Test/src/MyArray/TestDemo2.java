f71d46c168f8bbcca230031951f579c5be21bc45
patient-fyd <patient.fyd@gmail.com>
2023-01-16 23:00:48 +0800

    静态的泛型方法

--- Test/src/MyArray/TestDemo2.java
+++ Test/src/MyArray/TestDemo2.java
@@ -20,11 +20,29 @@ class Alg<T extends Comparable<T>> {
         return max;
     }
 }
+
+class Alg2<T> {
+    /**
+     * 静态的泛型方法
+     * @param array
+     * @return
+     * @param <T>
+     */
+    public static<T extends Comparable<T>>  T findMax(T[] array) {
+        T max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max.compareTo(array[i]) < 0) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
 public class TestDemo2 {
     public static void main(String[] args) {
-        Alg<Integer> alg = new Alg<Integer>();
+        Alg<Integer> alg2 = new Alg<Integer>();
         Integer[] array = {1,4,22,5,19};
-        Integer max = alg.findMax(array);
+        Integer max = alg2.findMax(array);
         System.out.println(max);
 
     }
