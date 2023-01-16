0877b5226c31d15b1819790b09851ccc2858fcc9
patient-fyd <patient.fyd@gmail.com>
2023-01-16 21:48:12 +0800

    Update TestDemo.java

--- Test/src/MyArray/TestDemo.java
+++ Test/src/MyArray/TestDemo.java
@@ -6,15 +6,20 @@ package MyArray;
  * @DateTime: 2023/1/16 21:05
  */
 
-class  MyArray {
-    public Object[] array = new Object[10];
+/**
+ * <T> 占位符，代表当前类是一个泛型类
+ * @param <T>
+ */
+class  MyArray<T> {
+    //public Object[] array = new Object[10];
 
+    public T[] array = (T[]) new Object[10];
     /**
      * 获取pos下标的值
      * @param pos
      * @return
      */
-    public Object getPos(int pos) {
+    public T getPos(int pos) {
         return array[pos];
     }
 
@@ -23,17 +28,26 @@ class  MyArray {
      * @param pos
      * @param val
      */
-    public void setPos(int pos,Object val){
+    public void setPos(int pos,T val){
         array[pos]=val;
     }
 }
 public class TestDemo {
     public static void main(String[] args) {
-        MyArray myArray = new MyArray();
-        myArray.setPos(0,1);
-        myArray.setPos(1,"hello");
+        /**
+         * 1、<Integer>指定当前类中，使用的类型是Integer类型
+         * 2、泛型帮我在编译期间做了两件事情
+         *  2.1 存放元素的时候，进行类型的检查
+         *  2.2 取元素的时候，进行类型的转换
+         * 3、<引用类型>
+         */
+        MyArray<Integer> myArray = new MyArray<Integer>();
 
-        String ret = (String)myArray.getPos(1);
+
+        myArray.setPos(0,1);
+        myArray.setPos(1,2);
+
+        Integer ret = myArray.getPos(1);
         System.out.println(ret);
     }
 }
