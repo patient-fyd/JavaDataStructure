3a1fb0430960180c0550c15000fdf2890c44f0a3
patient-fyd <patient.fyd@gmail.com>
2023-01-19 00:49:49 +0800

    单链表的简单创建

---
+++ Studying/src/MyList/MySingleList.java
@@ -0,0 +1,46 @@
+package MyList;
+
+import java.util.List;
+
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description:
+ * @DateTime: 2023/1/19 0:25
+ */
+public class MySingleList {
+
+    static class ListNode {
+        // 数值域
+        public int value;
+        // 下个个节点的地址
+        public ListNode next;
+
+        public ListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    /**
+     * 代表单链表的头节点的引用
+     */
+    public ListNode head;
+
+    /**
+     * 这里只是简单的进行链表的构造
+     */
+    public void createList() {
+        ListNode listNode1 = new ListNode(12);
+        ListNode listNode2 = new ListNode(17);
+        ListNode listNode3 = new ListNode(22);
+        ListNode listNode4 = new ListNode(43);
+        ListNode listNode5 = new ListNode(55);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        this.head = listNode1;
+    }
+
+}
---
+++ Studying/src/MyList/TestDemo.java
@@ -0,0 +1,16 @@
+package MyList;
+
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description:
+ * @DateTime: 2023/1/19 0:46
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        MySingleList mySingleList = new MySingleList();
+
+        mySingleList.createList();
+        System.out.println("--------------------------");
+
+    }
+}
