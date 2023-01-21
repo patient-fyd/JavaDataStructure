6b587fb6fda899fcc4e98c9c28985542f2ce3a8d
patient-fyd <patient.fyd@gmail.com>
2023-01-22 03:54:47 +0800

    返回链表的中间结点

--- Studying/src/MyList/MySingleList.java
+++ Studying/src/MyList/MySingleList.java
@@ -234,4 +234,20 @@ public class MySingleList {
         return head;
     }
 
+    /**
+     * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+     * 如果有两个中间结点，则返回第二个中间结点。
+     * 快慢指针
+     * @return 返回slow
+     */
+    public ListNode middleNode() {
+        ListNode slow = head;
+        ListNode fast = head;
+        if (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow  = slow.next;
+        }
+        return slow;
+    }
+
 }
