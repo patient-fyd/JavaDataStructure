1f96d75db924fcd81b6d898ccdf4ef5dc2e44f69
patient-fyd <patient.fyd@gmail.com>
2023-02-06 23:34:46 +0800

    Update Test9.java

--- Studying/src/TrainingTopic/list/Test9.java
+++ Studying/src/TrainingTopic/list/Test9.java
@@ -8,7 +8,7 @@ package TrainingTopic.list;
 public class Test9 {
     public static void main(String[] args) {
         System.out.println("llll");
-        System.out.println();
+        System.out.println(1+2);
     }
 
 }
