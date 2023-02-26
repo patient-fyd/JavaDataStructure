b06627b0d60a9e626edb5cee6c887af3e30b4f39
patient-fyd <patient.fyd@gmail.com>
2023-02-26 15:07:22 +0800

    Create Test10.java

---
+++ Studying/src/TrainingTopic/list/Test10.java
@@ -0,0 +1,12 @@
+package TrainingTopic.list;
+
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description: 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * @DateTime: 2023/2/26 15:06
+ */
+public class Test10 {
+    public ListNode detectCycle(ListNode head) {
+
+    }
+}
