9260425f60453c885f31d9f1e7c0e9bc8c3c9db4
patient-fyd <patient.fyd@gmail.com>
2023-02-26 14:23:10 +0800

    给你一个链表的头节点 head ，判断链表中是否有环。

--- Studying/src/TrainingTopic/list/Test9.java
+++ Studying/src/TrainingTopic/list/Test9.java
@@ -2,12 +2,23 @@ package TrainingTopic.list;
 
 /**
  * @Author: patient.fyd@gmail.com
- * @Description:
+ * @Description: 给你一个链表的头节点 head ，判断链表中是否有环。
  * @DateTime: 2023/1/31 23:28
  */
 public class Test9 {
-    public static void main(String[] args) {
-        
+    public boolean hasCycle(ListNode head) {
+        if(head == null) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
     }
-
 }
