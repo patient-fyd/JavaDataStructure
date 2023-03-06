9ec09759779f151f71e7ecb7c01126ee96d153b9
patient-fyd <patient.fyd@gmail.com>
2023-03-06 09:37:37 +0800

    栈，入栈操作的实现

---
+++ Studying/src/Stack/MyStack.java
@@ -0,0 +1,39 @@
+package Stack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description:
+ * @DateTime: 2023/3/6 9:29
+ */
+public class MyStack {
+    public int[] elem;
+    public int usedSize;
+
+    public MyStack(){
+        this.elem = new int[5];
+    }
+
+    /**
+     * 入栈
+     * @param val 入栈的元素
+     */
+    public void push(int val){
+        if (isFull()) {
+            this.elem = Arrays.copyOf(this.elem,2*this.elem.length);
+        }
+        this.elem[this.usedSize++] = val;
+    }
+
+    /**
+     * 判断当前栈是否为满
+     * @return 是返回true
+     */
+    public boolean isFull(){
+        return usedSize == elem.length;
+    }
+
+    
+}
---
+++ Studying/src/Stack/Test.java
@@ -0,0 +1,28 @@
+package Stack;
+
+import java.util.Stack;
+
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description:
+ * @DateTime: 2023/3/6 9:15
+ */
+public class Test {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        // 入栈
+        stack.push(12);
+        stack.push(23);
+        stack.push(34);
+        stack.push(45);
+        int x = stack.pop();
+        System.out.println(x);
+        // 获取栈顶元素，但是不删除
+        x = stack.peek();
+        System.out.println(x);
+        // 弹出一个元素，并且删除
+        x = stack.pop();
+        System.out.println(x);
+        boolean flg = stack.empty();
+    }
+}
